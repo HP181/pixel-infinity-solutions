173de8ad292530293e6aafdb719aaf8e5036fc09
Hitkumar <hit98987@gmail.com>
2024-04-14 17:53:51 -0400

    make about page responsive

--- app/about/page.js
+++ app/about/page.js
@@ -12,45 +12,45 @@ const page = () => {
               Skill, Technology, Innovation
             </h2>
             <p className=" mt-4 text-justify sm:text-center sm:pt-4 lg:w-2/3 mx-auto leading-relaxed text-base">
-              Codebucks is a well established IT company in poland.we offer
+              Pixel Infinity Solutions is a well established IT company in Canada. we offer
               website development services. we have work all over the world.
               Every website we using the latest method and technology.
             </p>
           </div>
           <div className="flex flex-wrap">
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6  border-gray-200 border-opacity-60">
+            <div className="  w-full lg:w-1/2 xl:w-1/4 px-8 py-6  border-gray-200 border-opacity-60">
               <h2 className=" text-center text-lg sm:text-xl text-gray-900 font-bold title-font mb-4">
                 Website Development
               </h2>
-              <p className="leading-relaxed text-base mb-4 text-center">
+              <p className="leading-relaxed text-justify text-base mb-4 sm:text-center">
                 we create and maintain Web application for your business.
               </p>
             </div>
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6  border-gray-200 border-opacity-60 lg:border-l-2">
+            <div className="w-full lg:w-1/2 xl:w-1/4 px-8 py-6  border-gray-200 border-opacity-60 lg:border-l-2">
               <h2 className=" text-center text-lg sm:text-xl text-gray-900 font-bold title-font mb-4">
                 Software Development
               </h2>
               <p className="leading-relaxed text-justify text-base mb-4 sm:text-center">
-                CodeBucks offers to create, design, deploy and
+              Pixel Infinity Solutions offers to create, design, deploy and
                 supports softwares that meet your needs.
               </p>
             </div>
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6  border-gray-200 border-opacity-60">
+            <div className="w-full lg:w-1/2 xl:w-1/4 xl:border-l-2 px-8 py-6  border-gray-200 border-opacity-60">
               <h2 className="text-lg sm:text-xl text-center text-gray-900 font-bold title-font mb-2">
                 E-Commerce Development
               </h2>
               <p className="leading-relaxed text-base mb-4 text-justify sm:text-center">
                 We create and implement E-commerce solutions that are integrated
-                with your online business . we will deliver
+                with your online business. we will deliver
                 your project on time and maintain quality.
               </p>
             </div>
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-gray-200 border-opacity-60 lg:border-l-2">
+            <div className="w-full lg:w-1/2 xl:w-1/4 px-8 py-6 border-gray-200 border-opacity-60 lg:border-l-2">
               <h2 className="text-lg sm:text-xl text-gray-900 font-bold title-font mb-2 text-center">
                 Website Disging
               </h2>
               <p className="leading-relaxed text-base mb-4 text-justify sm:text-center">
-                CodeBucks provide unique desging with fully responsive websites.
+                Pixel Infinity Solutions provide unique desging with fully responsive websites.
               </p>
             </div>
           </div>
