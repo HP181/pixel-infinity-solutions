5655660233ceba2090785df1e357230451655fab
Hitkumar <hit98987@gmail.com>
2024-04-24 23:58:39 -0400

    update texts and skills logo columns

--- app/_components/Skills.js
+++ app/_components/Skills.js
@@ -4,15 +4,15 @@ import Image from "next/image";
 const Skills = () => {
   return (
     <>
-      <div className="h-full  rounded-lg mt-10 sm:mt-16 pb-2">
+      <div className="h-full rounded-lg mt-10 sm:mt-16 pb-2">
         <p className="text-center pt-10 text-4xl  font-bold">Our Skills</p>
-        <div className="grid  xxs:grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5  m-10 h-full justify-evenly  place-items-center gap-y-12">
+        <div className="grid  grid-cols-2 gap-16 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5  m-10 h-full justify-evenly  place-items-center gap-y-12">
           {skillsData.map((item, index) => (
             <div
-              className="h-44 w-44  flex flex-col items-center justify-center"
+              className="h-36 w-36 sm:h-44 sm:w-44 p-2 flex flex-col items-center justify-center"
               key={index}
             >
-              <div className="h-28 w-28 border-2 shadow-xl  ">
+              <div className="h-20 w-20 sm:h-28 sm:w-28 border-2 shadow-xl  ">
                 <Image
                   width="900"
                   height="900"
--- app/_components/Technologies.js
+++ app/_components/Technologies.js
@@ -2,7 +2,7 @@ const Technologies = () => {
   return (
     <div className="mt-10">
       <h1 className=" text-center font-bold sm:mt-[4em] md:mt-32 text-4xl underline underline-offset-[16] mb-10 ">
-        our expertise
+        Our Expertise
       </h1>
       <div className=" grid place-items-center sm:grid-cols-2 sm:gap-10 xxs:gap-14 sm:w-full sm:h-full md:grid-cols-2 md:w-full md:h-full  lg:max-w-5xl lg:mx-auto lg:grid-cols-3 lg:mb-32">
         <div className="h-64 w-64  m-2 bg-slate-50 dark:bg-[#1d2432] rounded-lg shadow-2xl hover:scale-110 ease-out transition-all duration-500">
